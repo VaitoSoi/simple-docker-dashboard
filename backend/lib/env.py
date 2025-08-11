6d0d9466cb82bbbc59479a11ecb63a53be7580f3
VaitoSoi <duyduy15456@gmail.com>
2025-08-11 15:21:00 +0700

    change the database path

--- backend/lib/env.py
+++ backend/lib/env.py
@@ -2,7 +2,7 @@ import hashlib
 import os
 from uuid import uuid4
 
-DB_URL = os.getenv("DB_URL", "sqlite:///database.db")
+DB_URL = os.getenv("DB_URL", "sqlite:///data/database.db")
 SIGNATURE = os.getenv(
     "SIGNATURE", hashlib.sha256(uuid4().__str__().encode()).hexdigest()
 )
