91e95f217570fd9b576824d4f6da692af7acaa20
VaitoSoi <duyduy15456@gmail.com>
2025-08-11 15:28:42 +0700

    add volume page

---
+++ frontend/src/components/Volume/EntryExplorer.tsx
@@ -0,0 +1,302 @@
+import { useEffect, useState } from "react";
+import api from "../../lib/api";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import {
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import { HuhError } from "@/components/ui/icon";
+import { ChevronLeft, Download, FileWarning, RefreshCw } from "lucide-react";
+import { Skeleton } from "../ui/skeleton";
+import { Button } from "../ui/button";
+import { error, info } from "@/hooks/toasts";
+import { Checkbox } from "../ui/checkbox";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+
+interface VolumeEntryAPI {
+    name: string,
+    type:
+    | "directory"
+    | "file"
+    | "executable"
+    | "sock"
+    | "symlink"
+    | "other"
+}
+interface VolumeEntry extends VolumeEntryAPI {
+    isGoBack?: true
+}
+
+export default function ({ id }: { id: string }) {
+    const token = localStorage.getItem("token");
+
+    const [currentPath, setCurrentPath] = useState<string>("");
+    const [entries, setEntries] = useState<VolumeEntry[]>([]);
+
+    const [viewingFile, setViewingFile] = useState<boolean>(false);
+    const [fileTooBig, setFileTooBig] = useState<boolean>(false);
+
+    const [fileContent, setFileContent] = useState<string | null>(null);
+    const [enableJSONFormat, setEnableJSONFormat] = useState<boolean>(false);
+    const [jsonFormattedContent, setJSONFormattedContent] = useState<string | null>(null);
+
+    const [isRunningCommand, setIsRunningCommand] = useState<boolean>(false);
+
+    const [errored, setErrored] = useState<boolean>(false);
+
+    useEffect(() => void gotoEntry(), [currentPath]);
+    async function gotoEntry() {
+        try {
+            if (viewingFile) {
+                const response = await api.get<string>(
+                    `/docker/cat?id=${id}&path=${currentPath}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }
+                );
+                let content: string = response.data;
+                if (content.length >= 1_000_000) {
+                    setFileTooBig(true);
+                    content = content.slice(0, 1_000_000);
+                }
+                setFileContent(content);
+            } else {
+                const response = await api.get<VolumeEntryAPI[]>(
+                    `/docker/ls?id=${id}&path=${currentPath}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }
+                );
+                const entries: VolumeEntry[] = ([
+                    (currentPath != "" ? { name: "..", type: "", isGoBack: true } : undefined),
+                    ...response.data.map(entry => ({ ...entry, name: entry.name + (entry.type == "directory" ? "/" : "") }))
+                ].filter(val => !!val)) as VolumeEntry[];
+                setEntries(entries);
+            }
+        } catch (e) {
+            setErrored(true);
+            console.error(e);
+        }
+    }
+
+    async function download(path: string = currentPath) {
+        const pathFragment = path.split("/");
+        let filename = "";
+        if (pathFragment[pathFragment.length - 1] == "")
+            filename = `${pathFragment[pathFragment.length - 2]}.zip`;
+        else
+            filename = pathFragment[pathFragment.length - 1];
+
+        try {
+            const response = await api.get(
+                `/docker/download?id=${id}&path=${path}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    },
+                    responseType: "blob"
+                }
+            );
+            const url = window.URL.createObjectURL(response.data);
+            const a = document.createElement("a");
+            a.style = "display: none";
+            document.body.appendChild(a);
+            a.href = url;
+            a.download = filename;
+            a.click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+            setIsRunningCommand(false);
+        } catch (e) {
+            setErrored(true);
+            console.error(e);
+            if (e instanceof Error)
+                error(e.message);
+        }
+    }
+
+    const table = useReactTable<VolumeEntry>({
+        columns: [
+            {
+                id: "name",
+                header: "Name",
+                accessorKey: "name",
+                cell: ({ row }) => {
+                    const entry = row.original;
+                    const clickable =
+                        ["directory", "file", "symlink"].includes(entry.type) ||
+                        (entry.type == "executable" && entry.name.endsWith(".sh")) ||
+                        entry.isGoBack;
+                    return <p
+                        className={clickable && "hover:underline cursor-pointer"}
+                        onClick={() => {
+                            if (!clickable) return;
+                            if (
+                                ["file", "symlink"].includes(entry.type) ||
+                                (entry.type == "executable" && entry.name.endsWith(".sh"))
+                            )
+                                setViewingFile(true);
+                            setCurrentPath(oldPath =>
+                                entry.isGoBack
+                                    ? oldPath.split("/").slice(0, -2).join("/")
+                                    : oldPath + entry.name
+                            );
+                        }}
+                    >
+                        {row.original.name}
+                    </p>;
+                }
+            },
+            {
+                id: "type",
+                header: "Type",
+                accessorKey: "type"
+            },
+            {
+                id: "action",
+                header: "Action",
+                cell: ({ row }) =>
+                    row.original.isGoBack
+                        ? <></>
+                        : isRunningCommand
+                            ? <RefreshCw className="animate-spin" />
+                            : <Button
+                                variant="outline"
+                                onClick={() => {
+                                    if (isRunningCommand) return;
+                                    setIsRunningCommand(true);
+                                    download(currentPath + row.original.name);
+                                }}
+                            ><Download /></Button>
+            }
+        ],
+        data: entries,
+        getCoreRowModel: getCoreRowModel(),
+    });
+
+    return <>{
+        errored
+            ? <div className="rounded-md border p-20 flex flex-col items-center">
+                <HuhError />
+            </div>
+            : <div className="h-full rounded-md border p-4 pr-6 pl-6 flex flex-col">{
+                viewingFile
+                    ? <>
+                        <div className="flex flex-row items-center gap-2 border-b-2 pb-1">
+                            <ChevronLeft
+                                className="cursor-pointer"
+                                onClick={() => {
+                                    setCurrentPath(oldPath => oldPath.split("/").slice(0, -1).join("/"));
+                                    setViewingFile(false);
+                                    setFileContent(null);
+                                    setFileTooBig(false);
+
+                                    setEnableJSONFormat(false);
+                                    setJSONFormattedContent(null);
+                                }}
+                            />
+                            <p className="text-xl">{currentPath}</p>
+                            {currentPath.endsWith(".json") && fileContent != "" && !fileTooBig && <>
+                                <Checkbox
+                                    className="cursor-pointer"
+                                    checked={enableJSONFormat}
+                                    onCheckedChange={(checked) => {
+                                        setEnableJSONFormat(!!checked);
+                                        if (checked && jsonFormattedContent == null)
+                                            try {
+                                                const obj = JSON.parse(fileContent);
+                                                setJSONFormattedContent(JSON.stringify(obj, null, 4));
+                                            } catch (err) {
+                                                console.error(err);
+                                                if (err instanceof Error)
+                                                    error(err.message);
+                                            }
+                                    }}
+                                />
+                                <p>Pretty print</p>
+                            </>
+                            }
+                            {fileTooBig && <Tooltip>
+                                <TooltipTrigger><FileWarning /></TooltipTrigger>
+                                <TooltipContent><p>This file is too big to be displayed ;-;. To see full content, please download it</p></TooltipContent>
+                            </Tooltip>}
+                        </div>
+                        {fileContent != null || jsonFormattedContent != null
+                            ? <>{
+                                <div className="p-2 h-full w-full overflow-scroll">{
+                                    ((enableJSONFormat ? jsonFormattedContent : fileContent) || "<This is actually an empty file - This message is sent from this web, not from your app :D>")?.split("\n").map((line) =>
+                                        <p className="font-mono whitespace-pre">{line}</p>
+                                    )
+                                }</div>
+                            }</>
+                            : <>
+                                <Skeleton className="h-5 w-8/10 mt-2" />
+                                <Skeleton className="h-5 w-8/10 mt-2" />
+                                <Skeleton className="h-5 w-5/10 mt-2" />
+                            </>
+                        }</>
+                    : <Table>
+                        <TableHeader className="text-xl">
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <TableRow key={headerGroup.id} className={`w-1/${headerGroup.headers.length}`}>
+                                    {headerGroup.headers.map((header) =>
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    )}
+                                </TableRow>
+                            ))}
+                        </TableHeader>
+                        <TableBody className="text-xl">
+                            {table.getRowModel().rows?.length
+                                ? (
+                                    table.getRowModel().rows.map((row) => (
+                                        <TableRow
+                                            key={row.id}
+                                            data-state={row.getIsSelected() && "selected"}
+                                        >
+                                            {row.getVisibleCells().map((cell) => (
+                                                <TableCell key={cell.id}>
+                                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                </TableCell>
+                                            ))}
+                                        </TableRow>
+                                    ))
+                                )
+                                : (
+                                    <TableRow>
+                                        <TableCell colSpan={table.getAllColumns().length} className="h-24 text-center">
+                                            <div className="m-20">
+                                                <p className="text-center text-8xl m-4">🔍</p>
+                                                <p className="text-center text-2xl">No entry found</p>
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+            }</div>
+    }</>;
+}
+
+function useSelector(arg0: any) {
+    throw new Error("Function not implemented.");
+}
---
+++ frontend/src/components/Volumes/ListVolume.tsx
@@ -0,0 +1,261 @@
+import { useEffect, useState } from "react";
+import api from "../../lib/api";
+import type { APIVolume } from "@/lib/typing";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { AxiosError } from "axios";
+import {
+    flexRender,
+    getCoreRowModel,
+    getPaginationRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import {
+    ChevronLeft,
+    ChevronRight,
+    RefreshCw,
+    Trash2
+} from "lucide-react";
+import { HuhError } from "@/components/ui/icon";
+import { error, success } from "@/hooks/toasts";
+import { useNavigate } from "react-router";
+
+interface Volume extends APIVolume {
+    using: string[] | undefined
+}
+
+export default function () {
+    const token = localStorage.getItem("token");
+
+    const [containers, setContainers] = useState<Record<string, any>[] | false>(null);
+    const [volumes, setVolumes] = useState<Volume[]>([]);
+
+    const [errored, setErrored] = useState<boolean>(false);
+    const [isRunningCommand, setIsRunningCommand] = useState<boolean>(false);
+
+    useEffect(() => void getContainers(), []);
+    async function getContainers() {
+        try {
+            const response = await api.get<Record<string, any>[]>(
+                `/docker/containers?show_all=True&raw=True`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+
+            setContainers(response.data);
+        } catch (e) {
+            setErrored(true);
+            console.error(e);
+            if (e instanceof AxiosError && e.status == 403)
+                setContainers(false);
+        }
+    }
+
+    useEffect(() => void getVolumes(), [containers]);
+    async function getVolumes() {
+        if (containers == null) return;
+        try {
+            const response = await api.get<APIVolume[]>(
+                `/docker/volumes`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            );
+            const volumes: Volume[] = [];
+            if (containers)
+                for (const volume of response.data)
+                    volumes.push({
+                        ...volume,
+                        using: containers.filter((container) =>
+                            container["Mounts"]
+                                .filter((mount: Record<string, string>) =>
+                                    mount["Type"] == "volume" && mount["Name"] == volume.id
+                                )
+                                .length
+                        ).map(container => container["Name"]?.slice(1) || container["Id"])
+                    });
+            setVolumes(volumes);
+            setIsRunningCommand(false);
+        } catch (e) {
+            setErrored(true);
+            console.error(e);
+            if (!(e instanceof AxiosError))
+                console.error(e);
+        }
+    }
+
+    async function remove(id: string) {
+        try {
+            setIsRunningCommand(true);
+            await api.delete(`/docker/volume?id=${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            success("Removed volume");
+            getVolumes();
+        } catch {
+            setIsRunningCommand(false);
+            error("Can't remove volume D:");
+        }
+    }
+
+    async function prune() {
+        try {
+            setIsRunningCommand(true);
+            await api.delete(`/docker/volume/prune`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            success(`Pruned volumes`);
+            getVolumes();
+        } catch (err) {
+            setIsRunningCommand(false);
+            error("Can't prune volumes D:");
+        }
+    }
+
+    const table = useReactTable<Volume>({
+        columns: [
+            {
+                id: "id",
+                header: "ID",
+                accessorKey: "id",
+                cell: ({ getValue }) => <a
+                    href={`/volume/${getValue()}`}
+                    className="text-blue-500 hover:underline"
+                >{getValue()}</a>
+            },
+            {
+                id: "using",
+                header: "Using this volume",
+                cell: ({ row }) =>
+                    <div className="flex flex-row items-center gap-1">
+                        {row.original.using
+                            .slice(0, 3)
+                            .map((container) =>
+                                <a className="text-blue-500 hover:underline" href={`/container/${container}`}>
+                                    {container}
+                                </a>
+                            )}
+                        {row.original.using.length > 3 && "..."}
+                    </div>
+            },
+            {
+                id: "action",
+                header: "Action",
+                cell: ({ row }) => isRunningCommand
+                    ? <RefreshCw className="animate-spin" />
+                    : <Button
+                        variant="ghost"
+                        className={
+                            !row.original.using.length
+                                ? "hover:text-red-500"
+                                : "hover:text-gray-400"
+                        }
+                        onClick={() => !row.original.using.length && remove(row.original.id)}
+                    >
+                        <Trash2 className="size-6" />
+                    </Button>
+            }
+        ],
+        data: volumes,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+    });
+
+
+
+    return <>{
+        errored
+            ? <div className="rounded-md border p-20 flex flex-col items-center">
+                <HuhError />
+            </div>
+            : <div className="flex flex-col gap-5">
+                <div className="ml-auto flex flex-row items-center gap-5">
+                    <div className="flex flex-row">
+                        <Button
+                            variant="secondary"
+                            className="cursor-pointer"
+                            onClick={() => prune()}
+                        >
+                            <Trash2 />
+                            Prune unused
+                        </Button>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <Button variant="outline"
+                            onClick={() => table.previousPage()}
+                            disabled={!table.getCanPreviousPage()}
+                        ><ChevronLeft /> Previous</Button>
+                        <p className="ml-2 mr-3 text-lg">{table.getState().pagination.pageIndex + 1}/{table.getPageCount()}</p>
+                        <Button variant="outline"
+                            onClick={() => table.nextPage()}
+                            disabled={!table.getCanNextPage()}
+                        ><ChevronRight /> Next</Button>
+                    </div>
+                </div>
+                <div className="rounded-md border p-4 pr-6 pl-6">
+                    <Table>
+                        <TableHeader className="text-xl">
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <TableRow key={headerGroup.id} className={`w-1/${headerGroup.headers.length}`}>
+                                    {headerGroup.headers.map((header) =>
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    )}
+                                </TableRow>
+                            ))}
+                        </TableHeader>
+                        <TableBody className="text-xl">
+                            {table.getRowModel().rows?.length
+                                ? (
+                                    table.getRowModel().rows.map((row) => (
+                                        <TableRow
+                                            key={row.id}
+                                            data-state={row.getIsSelected() && "selected"}
+                                        >
+                                            {row.getVisibleCells().map((cell) => (
+                                                <TableCell key={cell.id}>
+                                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                </TableCell>
+                                            ))}
+                                        </TableRow>
+                                    ))
+                                )
+                                : (
+                                    <TableRow>
+                                        <TableCell colSpan={table.getAllColumns().length} className="h-24 text-center">
+                                            <div className="m-20">
+                                                <p className="text-center text-8xl m-4">🔍</p>
+                                                <p className="text-center text-2xl">No volume found</p>
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+                </div>
+            </div>
+    }</>;
+}
